749d9b1c7d5615cb701cc4bae0cd2b25b205a716
Ivan Kovic <ivan.kovic@seekandhit.com>
2018-09-18 15:07:40 +0200

    Add CSS reset, improve VS Code support for styled components, add base styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
+import { injectGlobal } from 'emotion';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'react-emotion';
 
+import { cssReset } from './styles';
+
+// apply CSS reset
+injectGlobal(cssReset);
+
 const Content = styled.div`
-  width: 100%;
-  margin: 24px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 24px;
 `;
 
 const App = () => (
